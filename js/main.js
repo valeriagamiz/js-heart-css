6c8cd656c2072cc465ed4200b5930c5ac50afe57
valeriagamiz <Valeria@Valerias-MacBook-Pro.local>
2016-03-08 16:38:11 -0500

    js

---
+++ js/main.js
@@ -0,0 +1,35 @@
+var $header = $('<header>');
+$header .addClass(".masthead");
+
+var $h1 = $('<h1>');
+$h1.removeClass(".big-heading") .addClass('.logo');
+
+var $main = $('<main>');
+$header .addClass(".wrapper");
+
+var $p = $('<p="first-child">');
+$p.hasClass("<main>") .addClass ('.slug-list');
+
+var $h2 = $('<h2>');
+$h2 .addClass(".slug-head");
+
+var $h2 = $('<h2>');
+$h2 .addClass(".slug-head");
+
+var $main = $('<main>');
+$main .addClass(".slug-list");
+
+var $li = $('<li="first-child>');
+$li .addClass(".slug-list-first");
+
+var $li = $('<li="last-child>');
+$li .addClass(".slug-list-last");
+
+var $dl = $('<dl>');
+$dl .addClass(".classification");
+
+var $dt = $('<dt>');
+$dt .removeClass(".big-lable");
+
+var $li = $('<li="last-child>');
+$li .show();
